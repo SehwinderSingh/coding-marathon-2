5849178f5fc68c933a209b0f7c6988637d59dcf5
Sehwinder Singh Mundra <sehwinder2014@gmail.com>
2026-09-24 15:17:30 +0300

    errors fixed

--- backend/api-server-starter/controllers/userControllers.js
+++ backend/api-server-starter/controllers/userControllers.js
@@ -1,34 +1,33 @@
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 
-
 const generateToken = (_id) => {
-    return jwt.sign( { _id}, process.env.SECRET, { expiresIn: "3d" });
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
 };
 
-const signupUser  = async (req, res) => {
-    const { name, email, password, phone_number, gender, date_of_birth, address } = req.body;
-    try {
-        const user = await User.signup(name, email, password, phone_number, gender, date_of_birth, address);
-        const token = generateToken(user);
-        res.status(201).json({ email, token });
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+const signupUser = async (req, res) => {
+  const { name, email, password, phone_number, gender, date_of_birth, address } = req.body;
+  try {
+    const user = await User.signup(name, email, password, phone_number, gender, date_of_birth, address);
+    const token = generateToken(user._id);
+    res.status(201).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 const loginUser = async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const user = await User.login(email, password);
-        const token = generateToken(user);
-        res.status(200).json({ email, token });
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const token = generateToken(user._id);
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = {
-    signupUser,
-    loginUser,
+  signupUser,
+  loginUser,
 };
