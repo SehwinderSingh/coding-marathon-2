3bc465a80b7e0fe584058e46a73bd0e42a696f88
Sehwinder Singh Mundra <sehwinder2014@gmail.com>
2026-09-24 11:51:21 +0300

    JobCRUD done

--- backend/api-server-starter/controllers/jobControllers.js
+++ backend/api-server-starter/controllers/jobControllers.js
@@ -5,8 +5,8 @@ const getAllJobs = async (req, res) => {
   try {
     const limit = parseInt(req.query._limit);
     const jobs = limit
-      ? (await Job.find({})).sort({ createdAt: -1 }).limit(limit)
-      : (await Job.find({})).sort({ createdAt: -1 });
+      ? await Job.find({}).limit(limit).sort({ createdAt: -1 })
+      : await Job.find({}).sort({ createdAt: -1 });
     res.status(200).json(jobs);
   } catch (error) {
     res.status(500).json({ message: "Failed to get jobs" });
@@ -15,33 +15,36 @@ const getAllJobs = async (req, res) => {
 
 const createJob = async (req, res) => {
   try {
-    const newJob = await Job.create({...req.body});
+    const newJob = await Job.create({ ...req.body });
     res.status(201).json(newJob);
   } catch (error) {
-    res.status(400).json({ message: "Failed to create job",error });
+    res.status(400).json({ message: "Failed to create job", error });
   }
 };
 
 const getJobById = async (req, res) => {
-    const {jobId} = req.params;
-    if (!mongoose.Types.ObjectId.isValid(jobId)) {
-        return res.status(400).json({ message: "Invalid job id" });
-    }
+  const { jobId } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(jobId)) {
+    return res.status(400).json({ message: "Invalid job id" });
+  }
   try {
     const newJob = await Job.findById(jobId);
-    if (!job )res.status(200).json(job);
+    if (!job) res.status(200).json(job);
   } catch (error) {
     res.status(500).json({ message: "Failed to get job by id" });
   }
 };
 
 const updateJob = async (req, res) => {
-    const {jobId} = req.params;
-    if (!mongoose.Types.ObjectId.isValid(jobId)) {
-        return res.status(400).json({ message: "Invalid job id" });
-    }
-    try {
-    const updatedJob = await Job.findByIdAndUpdate(jobId, req.body, {new: true, runValidators: true});
+  const { jobId } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(jobId)) {
+    return res.status(400).json({ message: "Invalid job id" });
+  }
+  try {
+    const updatedJob = await Job.findByIdAndUpdate(jobId, req.body, {
+      new: true,
+      runValidators: true,
+    });
     if (!updatedJob) return res.status(404).json({ message: "Job not found" });
     res.status(200).json(updatedJob);
   } catch (error) {
@@ -50,15 +53,16 @@ const updateJob = async (req, res) => {
 };
 
 const deleteJob = async (req, res) => {
-    const {jobId} = req.params;
-    if (!mongoose.Types.ObjectId.isValid(jobId)) {
-        return res.status(400).json({ message: "Invalid job id" });
-    }
-    try {
-    const deleteJob = await Job.findOneByIdAndDelete(jobId);
-    if (!deletejob) return res.status(404).json({ message: "Job not found" });
-    res.status(204).json({ message: "Job deleted successfully" });
+  const { jobId } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(jobId)) {
+    return res.status(400).json({ message: "Invalid job id" });
+  }
+  try {
+    const deletedJob = await Job.findByIdAndDelete(jobId);
+    if (deletedJob) {res.status(204).send("Job deleted successfully");}
+    else res.status(404).json({ message: "Job not found" });
   } catch (error) {
+    console.log(error);
     res.status(500).json({ message: "Failed to delete job" });
   }
 };
